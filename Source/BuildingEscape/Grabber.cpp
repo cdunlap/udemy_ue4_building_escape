c3f051f037e1d13bed27fa804f03b3eb89e07e09
Cale Dunlap <cale.dunlap@gmail.com>
2017-08-04 01:12:28 -0700

    Grabber picks up an actor and releases it based on key presses

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -21,9 +21,32 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	FindPhysicsHandleComponent();
+	SetupInputComponent();
+}
 
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	/// If we're carrying anything, update it
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		FVector viewPosition;
+		FRotator viewRotation;
+		GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(viewPosition, viewRotation);
+
+		FVector traceEnd = viewPosition + viewRotation.Vector() * Reach;
+
+		PhysicsHandle->SetTargetLocation(traceEnd);
+	}
+
+}
+
+void UGrabber::FindPhysicsHandleComponent()
+{
 	/// Attach physics handle
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 
@@ -37,31 +60,65 @@ void UGrabber::BeginPlay()
 	}
 }
 
-
-// Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::SetupInputComponent()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	// ...
+	/// Get the input component
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::GrabReleased);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Unable to find input component on %s"), *GetOwner()->GetName());
+	}
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
 	FVector viewPosition;
 	FRotator viewRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(viewPosition, viewRotation);
 
 	FVector traceEnd = viewPosition + viewRotation.Vector() * Reach; // FVector(0.0f, 0.0f, 20.0f);
-	DrawDebugLine(GetWorld(), viewPosition, traceEnd, FColor(255, 0, 0), false, 0.0f, 0.0f, 10.0f);
 
-	/// Trace out to the world to find a physics object
+																	 /// Trace out to the world to find a physics object
 	FCollisionQueryParams traceParams(FName(TEXT("")), false, GetOwner());
 	FHitResult hitResult;
 	GetWorld()->LineTraceSingleByObjectType(hitResult, viewPosition, traceEnd,
 		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
 		traceParams);
 
-	/// See what we hit
+	return hitResult;
+}
+
+void UGrabber::Grab()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grabber key pressed"));
+
+	/// Attempt to reach out to physics body with collision channel set
+	FHitResult hitResult = GetFirstPhysicsBodyInReach();
+
+	/// If we hit, attach a physics handle
+	UPrimitiveComponent * hitComponent = hitResult.GetComponent();
 	AActor * hitActor = hitResult.GetActor();
+
 	if (hitActor)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Grabber hit %s"), *hitActor->GetName());
+		
+		PhysicsHandle->GrabComponent(hitComponent, NAME_None, hitActor->GetActorLocation(), true);
 	}
 }
 
+void UGrabber::GrabReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grabber key released"));
+
+	/// Release physics handle
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		PhysicsHandle->ReleaseComponent();
+	}
+}
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -26,7 +27,23 @@ public:
 
 		
 private:
+	// How far ahead of the player we can reach (in cm)
 	float	Reach = 100.0f;
 
 	UPhysicsHandleComponent * PhysicsHandle = nullptr;
+	UInputComponent * InputComponent = nullptr;
+
+	// Raycast and grab what is in reach
+	void Grab();
+
+	// Called when the grab key is released
+	void GrabReleased();
+
+	// Find attached physic handle
+	void FindPhysicsHandleComponent();
+
+	// Setup assumed input component
+	void SetupInputComponent();
+
+	FHitResult GetFirstPhysicsBodyInReach() const;
 };
